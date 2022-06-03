27978a29e0831cb9e9514bca475cb5a06fe1042a
mariansc <104934131+mariansc@users.noreply.github.com>
2022-06-02 21:21:12 -0300

    att layout

--- view/alterarDadosCli.php
+++ view/alterarDadosCli.php
@@ -1,4 +1,3 @@
-<?php include_once 'navSair.php'?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -6,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../view/css/AlterarDados.css">
+    <link rel="stylesheet" href="../View/css/styledados.css">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <title>alterar dados do cliente</title>
@@ -71,15 +70,9 @@
                 </form>
                 <!-- ALTERAR DADOS CLIENTE -->
 
-            <?php
-        }
-        if(!$linhasProd == null) {
-            foreach ($linhasProd as $linhaProd) {
-            
-        ?>
-            <!-- ALTERAR DADOS PRODUTO -->
+                <!-- ALTERAR DADOS PRODUTO -->
 
-            <h1 class="alterar">Alterar dados do Produto</h1>
+                <h1 class="alterar">Alterar dados do Produto</h1>
                 <!-- CADASTRAR PRODUTO -->
 
                 <div class="col-12">
@@ -108,7 +101,7 @@
 
                         <div id="form-cadastro-produto" class="col-md-6">
                             <label for="cxProduto" class="form-label">Produto:</label>
-                            <input required type="text" class="form-control" name="nomeProduto" id="cxProduto">
+                            <input required type="text" class="form-control" name="nomeProduto" minlength="5" maxlength="15"id="cxProduto">
                         </div>
 
                         <div id="form-cadastro-preco" class="col-md-6">
@@ -130,8 +123,8 @@
                         </div>
 
                         <div id="form-cadastro-descricao" class="col-md-6">
-                            <label for="cxDescricao" class="form-label" minlength="5" maxlength="20">Descrição:</label>
-                            <input placeholder="Digite aqui..." type="text" class="form-control" style="height: 150px;" name="descricao" id="cxDescricao">
+                            <label for="cxDescricao" class="form-label">Descrição:</label>
+                            <input placeholder="Digite aqui..." type="text" class="form-control" style="height: 150px;" name="descricao" minlength="5" maxlength="20" id="cxDescricao"> 
                         </div>
 
 
@@ -146,52 +139,149 @@
                         <div id="img-container">
                             <img id="preview" src="" style="justify-content:center;">
                         </div>
-                        <!-- JS IMAGEM -->
-                        <script>
-                            function readImage() {
-                                if (this.files && this.files[0]) {
-                                    var file = new FileReader();
-                                    file.onload = function(e) {
-                                        document.getElementById("preview").src = e.target.result;
-                                    };
-                                    file.readAsDataURL(this.files[0]);
-                                }
-                            }
 
-                            document.getElementById("img-input").addEventListener("change", readImage, false);
-                        </script>
-                        <!-- JS IMAGEM -->
                         <a href="listaprodutos.html"> <button type="submit" class="btn btn-outline-primary btn-lg" style="float: right;" method="post">Cadastrar Produto</button> </a>
                     </form>
                 </div>
 
-                <!-- JS DIV CADASTRAR -->
 
-                <script>
-                    var btn = document.getElementById('btn-cad');
-                    var container = document.querySelector('.cont');
-                    btn.addEventListener('click', function() {
-
-                        if (cont.style.display === 'block') {
-                            cont.style.display = 'none';
-                        } else {
-                            cont.style.display = 'block';
-                        }
-                    });
-                </script>
-                <!-- JS DIV CADASTRAR -->
                 <!-- CADASTRAR PRODUTO -->
 
 
+            <?php
+        }
+        foreach ($linhasProd as $linhaProd) {
+
+            ?>
+                <div id="form-alterar">
+                    <table class="records">
+                        <thead>
+                            <tr>
+                                <th>Nome do Produto</th>
+                                <th> Valor produto</th>
+                                <th>Ação</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><?= $linhaProd['Nome do Produto'] ?></td>
+                                <td><?= $linhaProd['Preco'] ?></td>
+                                <td id="crud">
+                                    <button id="btn-div" type="submit" class="btn">visualizar</button>
+                                    <button id="btnAlterar" type="submit" class="btn">excluir</button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                </div>
+
+                <div id="container" class="container" style="display: none;">
+                    <form action="../controller/alterarDadosProd.php" method="post" class="row g-3" id="dados">
+                        <div id="form-cadastro-marca" class="col-md-6">
+                            <label for="cxMarca" class="form-label">Marca:</label>
+                            <input type="text" class="form-control" name="nomeMarca" id="cxMarca">
+                        </div>
+
+
+                        <div id="form-cadastro-produto" class="col-md-6">
+                            <label for="cxProduto" class="form-label">Produto:</label>
+                            <input type="text" class="form-control" name="nomeProduto" id="cxProduto" value="<?= $linhaProd['Nome do Produto'] ?>">
+                        </div>
+
+                        <div id="form-cadastro-preco" class="col-md-6">
+                            <label for="cxPreco" class="form-label">Preço do Lote: </label>
+                            <div class=" input-group" style="max-width: 400px;">
+                                <span class="input-group-text"> R$</span>
+                                <input type="number" step="0.01" name="preco" min="0.01" class="form-control" id="cxPreco" value="<?= $linhaProd['Preco'] ?>">
+                            </div>
+                        </div>
+
+
+                        <div id="form-cadastro-QIL" class="col-md-6">
+                            <label for="cxQIL" class="form-label">Quantidade de itens do lote:</label>
+                            <input type="number" class="form-control" name="QIL" id="cxQIL" value="<?= $linhaProd['Quantidade'] ?>">
+                        </div>
+
+                        <div id="form-cadastro-tamanho" class="col-md-6">
+                            <label for="cxTamanho" class="form-label">Tamanho dos itens:</label>
+                            <input type="text" class="form-control" name="tamanho" id="cxTamanho" value="<?= $linhaProd['Tamanho'] ?>">
+                        </div>
+
+                        <div id="form-cadastro-descricao" class="col-md-6">
+                            <label for="cxDescricao" class="form-label" minlength="5" maxlength="20">Descrição:</label>
+                            <input placeholder="Digite aqui..." type="text" class="form-control" style="height: 150px;" name="descricao" id="cxDescricao" value="<?= $linhaProd['Descricao'] ?>">
+                        </div>
+
+
+                        <div id="form-cadastro-disponivel" class="col-md-6">
+                            <label for="cxDisponivel" class="form-label">Lotes disponiveis:</label>
+                            <input type="number" class="form-control" name="disponivel" id="cxDisponivel" value="<?= $linhaProd['Disponivel'] ?>">
+                        </div>
+                        <div id="form-cadastro-imagem" class="col-md-6">
+                            <label for="lblImagem" class="form-label">Adicionar imagem do produto:</label>
+                            <input id="img-input" type="file" name="imagem" title="Usar arquivo com dimensões 300x300">
+                        </div>
+                        <div id="img-container">
+                            <img id="preview" src="" style="justify-content:center;">
+                        </div>
+                        <div class="col-12">
+                            <button id="btnAlterar" type="submit" class="btn"> Alterar dados</button>
+                        </div>
+                    </form>
+
     </main>
+<?php
+        }
+?>
+
+<!-- JS DIV CADASTRAR -->
+
+<script>
+    var btn = document.getElementById('btn-cad');
+    var container = document.querySelector('.cont');
+    btn.addEventListener('click', function() {
+
+        if (cont.style.display === 'block') {
+            cont.style.display = 'none';
+        } else {
+            cont.style.display = 'block';
+        }
+    });
+</script>
+<!-- JS DIV CADASTRAR -->
+
+<!-- JS IMAGEM -->
+<script>
+    function readImage() {
+        if (this.files && this.files[0]) {
+            var file = new FileReader();
+            file.onload = function(e) {
+                document.getElementById("preview").src = e.target.result;
+            };
+            file.readAsDataURL(this.files[0]);
+        }
+    }
+
+    document.getElementById("img-input").addEventListener("change", readImage, false);
+</script>
+<!-- JS IMAGEM -->
+
+<!-- JS ESCONDER DIV ALTERAR-->
+<script>
+    var btn = document.getElementById('btn-div');
+    var container = document.querySelector('.container');
+    btn.addEventListener('click', function() {
+
+        if (container.style.display === 'block') {
+            container.style.display = 'none';
+        } else {
+            container.style.display = 'block';
+        }
+    });
+</script>
+<!-- JS ESCONDER ALTERAR-->
+
 </body>
 
 </html>
-<?php
-        }
-    }
-    else
-        {
-            echo("<h2>Nenhum Produto cadastrado </h2>");
-        }
-        ?>
