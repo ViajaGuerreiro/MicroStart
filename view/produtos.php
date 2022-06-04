0a5a7ab72e525386e8230179d987267d62e7a214
mariansc <104934131+mariansc@users.noreply.github.com>
2022-06-03 21:30:49 -0300

    att layout

--- view/produtos.php
+++ view/produtos.php
@@ -31,11 +31,7 @@
         <!-- Carousel-->
 
         <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
+           
             <div class="carousel-inner">
                 <div class="carousel-item active">
                     <img src="./img/BANNERS 2/periféricos.png" class="d-block w-100" alt="...">
